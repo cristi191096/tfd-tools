f9f8b87369193035ac5b05d13a8f416f8d5cb506
Alex <cristi191096@gmail.com>
2024-07-13 20:18:37 +0100

    Added env

--- src/app/_actions/user.ts
+++ src/app/_actions/user.ts
@@ -2,14 +2,14 @@
 
 export async function searchUser(username: string, tag: string) {
 
-    const API_KEY = "test_5e27611acc2fa45d8233cd1d49b596dcb01cdb693b92c72ddea6455fc22789dbefe8d04e6d233bd35cf2fabdeb93fb0d";    
+  
     
     console.log("user: ", username)
     console.log("tag: ", tag)
     const response = await fetch(`https://open.api.nexon.com/tfd/v1/id?user_name=${username}%23${tag}`, {
         method: "GET",
         headers:{
-          "x-nxopen-api-key": API_KEY   
+          "x-nxopen-api-key": process.env.NEXT_PUBLIC_NEXON_API_KEY! 
         }
     })
     const ouidJson = await response.json()
@@ -17,7 +17,7 @@ export async function searchUser(username: string, tag: string) {
     const basicInfoRes = await fetch(`https://open.api.nexon.com/tfd/v1/user/basic?ouid=${ouidJson.ouid}`, {
         method: "GET",
         headers:{
-          "x-nxopen-api-key": API_KEY   
+          "x-nxopen-api-key": process.env.NEXT_PUBLIC_NEXON_API_KEY!   
         }
     })
 
